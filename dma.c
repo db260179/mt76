b1fe0bc5fe72776ffe54e98b479376a7232028e9
David Bentham <db260179@gmail.com>
2021-01-30 13:38:08 +0000

    mt76: use threaded NAPI

--- dma.c
+++ dma.c
@@ -545,7 +545,7 @@ mt76_dma_init(struct mt76_dev *dev)
 	init_dummy_netdev(&dev->napi_dev);
 
 	for (i = 0; i < ARRAY_SIZE(dev->q_rx); i++) {
-		netif_napi_add(&dev->napi_dev, &dev->napi[i], mt76_dma_rx_poll,
+		netif_threaded_napi_add(&dev->napi_dev, &dev->napi[i], mt76_dma_rx_poll,
 			       64);
 		mt76_dma_rx_fill(dev, &dev->q_rx[i]);
 		skb_queue_head_init(&dev->rx_skb[i]);
