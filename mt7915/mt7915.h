174aa92fe5df12b3e673cf861b3cf4d30a88b6d6
David Bentham <dbentham@fgs-uk.com>
2026-02-20 10:10:09 +0000

    mt76: mt7915: add muru user number debug command

--- mt7915/mt7915.h
+++ mt7915/mt7915.h
@@ -685,6 +685,7 @@ int mt7915_mcu_set_pulse_th(struct mt7915_dev *dev,
 int mt7915_mcu_set_radar_th(struct mt7915_dev *dev, int index,
 			    const struct mt7915_dfs_pattern *pattern);
 int mt7915_mcu_set_muru_ctrl(struct mt7915_dev *dev, u32 cmd, u32 val);
+int mt7915_set_muru_cfg(struct mt7915_phy *phy, u8 mode, u8 val);
 int mt7915_mcu_apply_group_cal(struct mt7915_dev *dev);
 int mt7915_mcu_apply_tx_dpd(struct mt7915_phy *phy);
 int mt7915_mcu_get_chan_mib_info(struct mt7915_phy *phy, bool chan_switch);
