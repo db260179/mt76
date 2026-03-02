724965ec997f6ff55214d5c351ae67cc296b65c7
Roopni Devanathan <quic_rdevanat@quicinc.com>
2026-03-02 15:43:20 +0000

    wifi: cfg80211/mac80211: Add support to get radio index
    Currently, per-radio attributes are set on per-phy basis, i.e., all the
    radios present in a wiphy will take attributes values sent from user. But
    each radio in a wiphy can get different values from userspace based on
    its requirement.
    
    To extend support to set per-radio attributes, add support to get radio
    index from userspace. Add an NL attribute - NL80211_ATTR_WIPHY_RADIO_INDEX,
    to get user specified radio index for which attributes should be changed.
    Pass this to individual drivers, so that the drivers can use this radio
    index to change per-radio attributes when necessary. Currently, per-radio
    attributes identified are:
    NL80211_ATTR_WIPHY_TX_POWER_LEVEL
    NL80211_ATTR_WIPHY_ANTENNA_TX
    NL80211_ATTR_WIPHY_ANTENNA_RX
    NL80211_ATTR_WIPHY_RETRY_SHORT
    NL80211_ATTR_WIPHY_RETRY_LONG
    NL80211_ATTR_WIPHY_FRAG_THRESHOLD
    NL80211_ATTR_WIPHY_RTS_THRESHOLD
    NL80211_ATTR_WIPHY_COVERAGE_CLASS
    NL80211_ATTR_TXQ_LIMIT
    NL80211_ATTR_TXQ_MEMORY_LIMIT
    NL80211_ATTR_TXQ_QUANTUM
    
    By default, the radio index is set to -1. This means the attribute should
    be treated as a global configuration. If the user has not specified any
    index, then the radio index passed to individual drivers would be -1. This
    would indicate that the attribute applies to all radios in that wiphy.
    
    Signed-off-by: Roopni Devanathan <quic_rdevanat@quicinc.com>
    Link: https://patch.msgid.link/20250615082312.619639-2-quic_rdevanat@quicinc.com
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- mt7915/main.c
+++ mt7915/main.c
@@ -449,7 +449,8 @@ out:
 	return err;
 }
 
-static int mt7915_config(struct ieee80211_hw *hw, u32 changed)
+static int mt7915_config(struct ieee80211_hw *hw, int radio_idx,
+			 u32 changed)
 {
 	struct mt7915_dev *dev = mt7915_hw_dev(hw);
 	struct mt7915_phy *phy = mt7915_hw_phy(hw);
@@ -934,7 +935,8 @@ static void mt7915_tx(struct ieee80211_hw *hw,
 	mt76_tx(mphy, control->sta, wcid, skb);
 }
 
-static int mt7915_set_rts_threshold(struct ieee80211_hw *hw, u32 val)
+static int mt7915_set_rts_threshold(struct ieee80211_hw *hw, int radio_idx,
+				    u32 val)
 {
 	struct mt7915_dev *dev = mt7915_hw_dev(hw);
 	struct mt7915_phy *phy = mt7915_hw_phy(hw);
@@ -1130,7 +1132,8 @@ mt7915_offset_tsf(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
 }
 
 static void
-mt7915_set_coverage_class(struct ieee80211_hw *hw, s16 coverage_class)
+mt7915_set_coverage_class(struct ieee80211_hw *hw, int radio_idx,
+			  s16 coverage_class)
 {
 	struct mt7915_phy *phy = mt7915_hw_phy(hw);
 	struct mt7915_dev *dev = phy->dev;
@@ -1142,7 +1145,7 @@ mt7915_set_coverage_class(struct ieee80211_hw *hw, s16 coverage_class)
 }
 
 static int
-mt7915_set_antenna(struct ieee80211_hw *hw, u32 tx_ant, u32 rx_ant)
+mt7915_set_antenna(struct ieee80211_hw *hw, int radio_idx, u32 tx_ant, u32 rx_ant)
 {
 	struct mt7915_dev *dev = mt7915_hw_dev(hw);
 	struct mt7915_phy *phy = mt7915_hw_phy(hw);
@@ -1720,7 +1723,7 @@ mt7915_twt_teardown_request(struct ieee80211_hw *hw,
 }
 
 static int
-mt7915_set_frag_threshold(struct ieee80211_hw *hw, u32 val)
+mt7915_set_frag_threshold(struct ieee80211_hw *hw, int radio_idx, u32 val)
 {
 	return 0;
 }
